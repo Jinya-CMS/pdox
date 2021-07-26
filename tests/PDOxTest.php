fdb0f3ec82f5ddfe0dd49a402e5b91c49473f986
Imanuel Ulbricht <me@imanuel.dev>
2021-07-26 19:00:50 +0200

    #1 Wrap PDO and expose all functions
    Fixed psalm and phpstan issues

--- tests/PDOxTest.php
+++ tests/PDOxTest.php
@@ -8,19 +8,23 @@ use PHPUnit\Framework\TestCase;
 class PDOxTest extends TestCase
 {
 
-    public function test__construct()
+    public function test__construct(): void
     {
         $pdo = new PDOx('sqlite::memory:');
         $pdo->exec('CREATE TABLE test (pkey int primary key)');
         $pdo->exec('INSERT INTO test (pkey) VALUES (1)');
-        $data = $pdo->query('SELECT * FROM test');
+        $stmt = $pdo->query('SELECT * FROM test');
 
-        $this->assertNotFalse($data);
+        $this->assertNotFalse($stmt);
 
-        $result = $data->fetch(PDOx::FETCH_ASSOC);
-        $this->assertNotFalse($result);
+        if ($stmt !== false) {
+            $result = $stmt->fetch(PDOx::FETCH_ASSOC);
+            $this->assertNotFalse($result);
 
-        $this->assertArrayHasKey('pkey', $result);
-        $this->assertEquals(1, $result['pkey']);
+            if ($result !== false) {
+                $this->assertArrayHasKey('pkey', $result);
+                $this->assertEquals(1, $result['pkey']);
+            }
+        }
     }
 }
